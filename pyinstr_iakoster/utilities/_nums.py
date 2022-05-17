82ef3c8113c5617c572879b5b804dbf34d3057e4
Ivan Kosternoi <ivan.kosternoy@gmail.com>
2022-05-17 09:54:04 +0300

    feat(utilities): add function for converting from some base to integer with base 10

--- pyinstr_iakoster/utilities/_nums.py
+++ pyinstr_iakoster/utilities/_nums.py
@@ -1,11 +1,15 @@
 import string
 
 
+__all__ = ["num_sign", "to_base", "from_base"]
+
+
 def num_sign(value: int | float, pos_zero: bool = False) -> int:
     """
     Get sign of the value.
 
-    Returns +1 if value is positive, -1 if negative and 0 if value is zero.
+    Returns +1 if value is positive, -1 - if negative and
+    0 -- if value is zero.
 
     Parameters
     ----------
@@ -25,13 +29,13 @@ def num_sign(value: int | float, pos_zero: bool = False) -> int:
     return sign
 
 
-def to_base(val: int, base: int) -> str:
+def to_base(value: int, base: int) -> str:
     """
     Convert integer with base 10 to selected base.
 
     Parameters
     ----------
-    val: int
+    value: int
         value for convertig.
     base: int
         integer base.
@@ -44,23 +48,67 @@ def to_base(val: int, base: int) -> str:
     Raises
     ------
     ValueError
-        if base not in range [2; 36]
+        if base not in range [2; 36].
     """
     if not 1 < base < 37:
         raise ValueError("base must be in range [2; 36]")
 
-    if val == 0:
+    if value == 0:
         return "0"
-    digits = string.digits + string.ascii_letters
-    sign = num_sign(val)
-    val = abs(val)
+    digits = string.digits + string.ascii_lowercase
+    sign = num_sign(value)
+    value = abs(value)
     val_digits = []
 
-    while val:
-        val, mod = divmod(val, base)
+    while value:
+        value, mod = divmod(value, base)
         val_digits.append(digits[mod])
     if sign < 0:
         val_digits.append('-')
     val_digits.reverse()
 
     return ''.join(val_digits)
+
+
+def from_base(value: str, base: int) -> int:
+    """
+    Convert integer with some base 10 to integer with base 10.
+
+    Parameters
+    ----------
+    value: str
+        value for converting.
+    base: int
+        base of the value.
+
+    Returns
+    -------
+    int
+        converted value.
+
+    Raises
+    ------
+    ValueError
+        if base not in range [2; 36].
+    """
+    if not 1 < base < 37:
+        raise ValueError("base must be in range [2; 36]")
+
+    if value == "0":
+        return 0
+    if value[0] == "-":
+        negative = True
+        value = value[1:].lower()
+    else:
+        negative = False
+        value = value.lower()
+
+    digits = string.digits + string.ascii_lowercase
+    int_value = 0
+
+    for i_digit, digit in enumerate(value[::-1]):
+        int_value += digits.index(digit) * base ** i_digit
+
+    if negative:
+        return ~int_value + 1
+    return int_value
--- tests/test_utilities/test_nums.py
+++ tests/test_utilities/test_nums.py
@@ -2,35 +2,71 @@ import unittest
 
 import random
 
-from pyinstr_iakoster.utilities import to_base
+from pyinstr_iakoster.utilities import to_base, from_base
 
 
 class TestBaseOperations(unittest.TestCase):
 
-    chr_ids = list(range(48, 58)) + list(range(97, 123))
+    chr_ids = [chr(c) for c in list(range(48, 58)) + list(range(97, 123))]
 
     def test_to_base(self):
         self.assertListEqual(
             [to_base(i, 36) for i in range(0, 36)],
-            [chr(i) for i in self.chr_ids]
+            self.chr_ids
         )
 
     def test_to_base_negatives(self):
         self.assertListEqual(
             [to_base(i, 36) for i in range(-35, 0)],
-            ["-" + chr(i) for i in self.chr_ids[1:][::-1]]
+            ["-" + i for i in self.chr_ids[1:][::-1]]
         )
 
     def test_to_base_large(self):
-        values = [int(random.uniform(36, 10000)) for _ in range(100)]
+        values = [int(random.uniform(0, 10000)) for _ in range(100)]
         self.assertListEqual(
             [to_base(v, 16) for v in values],
             [hex(v)[2:] for v in values]
         )
+        values = [int(random.uniform(-10000, -10)) for _ in range(100)]
+        self.assertListEqual(
+            [to_base(v, 16) for v in values],
+            ["-" + hex(v)[3:] for v in values]
+        )
 
     def test_to_base_exceptions(self):
-        with self.assertRaises(ValueError) as exc:
+        with self.assertRaises(ValueError):
             to_base(0, 37)
+        with self.assertRaises(ValueError) as exc:
+            to_base(0, 1)
+        self.assertEqual(
+            exc.exception.args[0],
+            "base must be in range [2; 36]"
+        )
+
+    def test_from_base(self):
+        self.assertListEqual(
+            [from_base(i, 36) for i in self.chr_ids],
+            list(range(36))
+        )
+
+    def test_from_base_negatives(self):
+        self.assertListEqual(
+            [from_base("-" + i, 36) for i in self.chr_ids],
+            list(range(0, -36, -1))
+        )
+
+    def test_from_base_large(self):
+        values = [int(random.uniform(-10000, 10000)) for _ in range(100)]
+        self.assertListEqual(
+            [from_base(to_base(v, 36), 36) for v in values],
+            values
+        )
+
+    def test_from_base_exceptions(self):
+        with self.assertRaises(ValueError):
+            from_base("0", 37)
+        with self.assertRaises(ValueError) as exc:
+            from_base("0", 1)
         self.assertEqual(
             exc.exception.args[0],
             "base must be in range [2; 36]"
