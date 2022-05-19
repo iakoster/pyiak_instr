7ac542b77a7123fc14d70a9241588f8c96e00eef
Ivan Kosternoi <ivan.kosternoy@gmail.com>
2022-05-19 14:43:18 +0300

    chore(NoSql): add docstrings

--- pyinstr_iakoster/rwfile/_rwnosql.py
+++ pyinstr_iakoster/rwfile/_rwnosql.py
@@ -17,9 +17,30 @@ __all__ = [
 
 
 class RWNoSqlTable(Table):
+    """
+    Represents a single modified TinyDB table.
 
-    def rwf_insert(self, **doc_fields: Any) -> int:
-        return Table.insert(self, doc_fields)
+    See Also
+    --------
+    tinydb.table.Table: parent class.
+
+    """
+
+    def rwf_insert(self, **fields: Any) -> int:
+        """
+        Insert a new document into the table.
+
+        Parameters
+        ----------
+        **fields: Any
+            parameters for new document.
+
+        Returns
+        -------
+        int
+            the inserted document's ID.
+        """
+        return Table.insert(self, fields)
 
     def rwf_update(
             self,
@@ -27,15 +48,63 @@ class RWNoSqlTable(Table):
             doc_ids: Iterable[int] | int = None,
             **fields: Any
     ) -> list[int]:
+        """
+        Update all matching documents to have a given set of fields.
+
+        Parameters
+        ----------
+        cond: QueryLike
+            which documents to update.
+        doc_ids: iterable if ints or int
+            document ID('s) to update.
+        **fields: Any
+            the fields that the matching documents will have or a method
+            that will update the documents.
+
+        Returns
+        -------
+        list of ints
+            the updated document's ID.
+        """
         if isinstance(doc_ids, int):
             doc_ids = (doc_ids,)
         return Table.update(self, fields, cond=cond, doc_ids=doc_ids)
 
     def rwf_upsert(self, cond: QueryLike = None, **fields) -> list[int]:
+        """
+        Update documents, if they exist, insert them otherwise.
+
+        Parameters
+        ----------
+        cond: QueryLike
+            which document to look for.
+        **fields: Any
+            the document parameters to insert or the fields to update.
+
+        Returns
+        -------
+        list of ints
+            the updated documents' IDs.
+
+        See Also
+        --------
+        tinydb.table.Table.upsert: original method.
+
+        """
         return Table.upsert(self, fields, cond=cond)
 
 
 class RWNoSqlJsonDatabase(TinyDB):
+    """
+    The class of the modified TinyDB.
+
+    Aimed to work only with JSON files.
+
+    See Also
+    --------
+    tinydb.TinyDB: parent class.
+
+    """
 
     table_class = RWNoSqlTable
     _tables: dict[str, table_class]
@@ -54,6 +123,24 @@ class RWNoSqlJsonDatabase(TinyDB):
     rwf_upsert = RWNoSqlTable.rwf_upsert
 
     def table(self, name: str, **kwargs: Any) -> table_class:
+        """
+        Get access to a RWNoSqlTable.
+
+        Parameters
+        ----------
+        name: str
+            The name of the table.
+        **kwargs: Any
+            Keyword arguments to pass to the table class constructor.
+
+        Returns
+        -------
+
+        See Also
+        --------
+        tinydb.TinyDB: original method.
+
+        """
         if name in self._tables:
             return self._tables[name]
         table = self.table_class(self.storage, name, **kwargs)
@@ -62,7 +149,21 @@ class RWNoSqlJsonDatabase(TinyDB):
 
     @property
     def filepath(self):
+        """
+        Returns
+        -------
+        Path
+            path to the JSON database/file.
+        """
         return self._filepath
 
     def __getitem__(self, table: str) -> table_class:
+        """
+        Get access to a RWNoSqlTable.
+
+        See Also
+        --------
+        self.table: this magic uses the specified method.
+
+        """
         return self.table(table)
