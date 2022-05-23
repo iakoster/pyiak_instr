56b0a60430affe381a209f3f1f9a209cd2d20675
Ivan Kosternoi <ivan.kosternoy@gmail.com>
2022-05-23 09:48:37 +0300

    feat(Fields): add base field exception

--- pyinstr_iakoster/exceptions/_comm.py
+++ pyinstr_iakoster/exceptions/_comm.py
@@ -2,32 +2,39 @@ from ._base import PyiError
 
 
 __all__ = [
+    "FieldError",
     "FloatWordsCountError",
     "PartialFieldError"
 ]
 
 
-class FloatWordsCountError(PyiError):
+class FieldError(PyiError):
+
+    def __init__(self, msg: str, field: str, *args):
+        PyiError.__init__(self, msg)
+        self.field = field
+        self.args = (self.message, field, *args)
+
+
+class FloatWordsCountError(FieldError):
 
     def __init__(self, field: str, expected: int, got: int | float):
-        PyiError.__init__(
+        FieldError.__init__(
             self,
             f"not integer count of words in the {field}. "
-            f"Expected {expected}, got {got:.1f}"
+            f"Expected {expected}, got {got:.1f}",
+            field, expected, got
         )
-        self.field = field
         self.expected = expected
         self.got = got
-        self.args = (self.message, self.field, self.expected, self.got)
 
 
-class PartialFieldError(PyiError):
+class PartialFieldError(FieldError):
 
     def __init__(self, field: str, occup: int | float):
-        PyiError.__init__(
+        FieldError.__init__(
             self,
-            f"The {field} is incomplete. The field is filled to {occup:.1f}"
+            f"The {field} is incomplete. The field is filled to {occup:.1f}",
+            field, occup
         )
-        self.field = field
         self.occupancy = occup
-        self.args = (self.message, self.field, self.occupancy)
