75a4e77dafc5c15ce6c2dc1acf17767286bde22f
Ivan Kosternoi <ivan.kosternoy@gmail.com>
2022-06-06 10:21:09 +0300

    feat(Message): add specific exception for message

--- pyinstr_iakoster/exceptions/__init__.py
+++ pyinstr_iakoster/exceptions/__init__.py
@@ -2,6 +2,9 @@ from ._base import *
 from ._log import *
 from ._rwfile import *
 from ._comm import (
+    MessageError,
+    NotConfiguredMessageError,
+    FieldError,
     FloatWordsCountError,
     PartialFieldError,
 )
--- pyinstr_iakoster/exceptions/_comm.py
+++ pyinstr_iakoster/exceptions/_comm.py
@@ -1,8 +1,9 @@
 from ._base import PyiError
-import configparser
 
 
 __all__ = [
+    "MessageError",
+    "NotConfiguredMessageError",
     "FieldError",
     "FloatWordsCountError",
     "PartialFieldError"
@@ -39,3 +40,21 @@ class PartialFieldError(FieldError):
             field, occup
         )
         self.occupancy = occup
+
+
+class MessageError(PyiError):
+
+    def __init__(self, msg: str, message: str, *args):
+        PyiError.__init__(self, msg)
+        self.message_class = message
+        self.args = (self.message, message, *args)
+
+
+class NotConfiguredMessageError(MessageError):
+
+    def __init__(self, message: str):
+        MessageError.__init__(
+            self,
+            "fields in %s instanse not configured" % message,
+            message
+        )
