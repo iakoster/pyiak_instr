d5817245a2dbfaf961322d7bf76abbc1a70ca400
Ivan Kosternoi <ivan.kosternoi@gmail.com>
2022-10-22 18:52:47 +0300

    feat(Connection): create basic Connection methods

--- pyinstr_iakoster/communication/_con/_con.py
+++ pyinstr_iakoster/communication/_con/_con.py
@@ -1,8 +1,10 @@
 import logging
+import datetime as dt
 from typing import Any
 
 from .._msg import Message, MessageContentError
 from .._pf import MessageErrorMark
+from ...core import Code
 
 
 class Connection(object):  # nodesc
@@ -27,6 +29,9 @@ class Connection(object):  # nodesc
             self._addr = None
         else:
             self.bind(address)
+
+        self._tx_to = dt.timedelta(seconds=15)
+        self._rx_to = dt.timedelta(seconds=5)
 
     def close(self) -> None:
         raise NotImplementedError()
@@ -69,6 +74,23 @@ class Connection(object):  # nodesc
             )
         return answer
 
+    def set_timeouts(
+            self,
+            transmit_timeout: dt.datetime | int = 15,
+            receive_timeout: dt.datetime | int = 5,
+    ):
+        if isinstance(transmit_timeout, int):
+            transmit_timeout = dt.timedelta(seconds=transmit_timeout)
+        if isinstance(receive_timeout, int):
+            receive_timeout = dt.timedelta(seconds=receive_timeout)
+
+        self._tx_to = transmit_timeout
+        self._rx_to = receive_timeout
+
+    def _log_info(self, entry: str) -> None:
+        if self._logger is not None:
+            self._logger.info(entry)
+
     def _read(self, msg: Message, emark: MessageErrorMark) -> Message:
         if not msg.splitable:
             return self._send(msg, emark)
@@ -80,7 +102,90 @@ class Connection(object):  # nodesc
         return answer
 
     def _send(self, msg: Message, emark: MessageErrorMark) -> Message:
-        ...
+
+        received = 0
+        invalid_received = 0
+        transmitted = 0
+        transmit_start = dt.datetime.now()
+
+        while transmit_start - dt.datetime.now() < self._tx_to:
+
+            self._transmit(msg)
+            transmitted += 1
+            self._log_info(repr(msg))
+            receive_start = dt.datetime.now()
+
+            while receive_start - dt.datetime.now() < self._rx_to:
+
+                try:
+                    ans, src_addr = self._receive()
+                    received += 1
+                    self._log_info("{}, src={}, dst={}".format(
+                        ans.hex(" "), src_addr, self._addr
+                    ))
+
+                except TimeoutError as exc:
+                    self._log_info(repr(exc))
+
+                else:
+                    if msg.rx != src_addr:
+                        invalid_received += 1
+                        self._log_info(
+                            f"message received from {src_addr}, "
+                            f"but expected from {msg.rx}"
+                        )
+                        continue
+
+                    ans, code = self._validate_bytes_message(msg, ans, emark)
+                    if code == Code.ERROR:
+                        continue
+
+                    code = self._validate_message(ans, emark)
+                    self._log_info(repr(ans))
+
+                    if code == Code.OK:
+                        return ans
+                    elif code == Code.WAIT:
+                        receive_start = dt.datetime.now()
+                    else:
+                        invalid_received += 1
+                        self._log_info(f"receive with code: {repr(code)}")
+
+        raise ConnectionError(
+            "time is up: "
+            "received={}, invalid_received={}, transmitted={}".format(
+                received, invalid_received, transmitted
+            )
+        )
+
+    def _validate_bytes_message(
+            self,
+            tx_msg: Message,
+            rx_msg: bytes,
+            emark: MessageErrorMark,
+    ) -> tuple[Message, Code]:
+        emark_exists = False
+        if emark.bytes_required:
+            rx_msg, emark_exists = emark.exists(rx_msg)
+        # todo: check that bytes can be reformatted into a message
+        rx_msg = tx_msg.get_same_instance().extract(rx_msg)\
+            .set_addresses(tx=tx_msg.rx, rx=self._addr)
+
+        if emark_exists:
+            return rx_msg, Code.ERROR
+        return rx_msg, Code.OK
+
+    def _validate_message(
+            self, rx_msg: Message, emark: MessageErrorMark
+    ) -> Code:
+        emark_exists = False
+        if not emark.bytes_required:
+            _, emark_exists = emark.exists(rx_msg)
+        # todo: check code in ResponseField (if exists)
+
+        if emark_exists:
+            return Code.ERROR
+        return Code.OK
 
     def _write(self, msg: Message, emark: MessageErrorMark) -> Message:
         if not msg.splitable:
@@ -93,9 +198,17 @@ class Connection(object):  # nodesc
         return answer
 
     @property
-    def address(self) -> Any:
+    def address(self) -> ADDRESS_TYPE:
         return self._addr
 
     @property
     def hapi(self):
         return self._hapi
+
+    @property
+    def receive_timeout(self) -> dt.timedelta:
+        return self._rx_to
+
+    @property
+    def transmit_timeout(self) -> dt.timedelta:
+        return self._tx_to
