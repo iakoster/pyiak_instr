8cb139a9914b45a9785b3f1960c03d71942183b0
Ivan Kosternoi <iakoster.42@gmail.com>
2022-11-19 14:48:22 +0300

    chore(RWNoSqlJsonDatabase): delete nosql (TinyDB) database class instance.
    This is done because the class is not needed in fact.

--- pyinstr_iakoster/rwfile/__init__.py
+++ pyinstr_iakoster/rwfile/__init__.py
@@ -1,14 +1,11 @@
 from ._core import RWFile, RWFileError, FileSuffixError
 from ._rwconfig import RWConfig
 from ._rwsqllite import RWSQLite
-from ._rwnosql import RWNSDocument, RWNoSqlJsonDatabase
 
 
 __all__ = [
     "RWFile",
     "RWConfig",
-    "RWNSDocument",
-    "RWNoSqlJsonDatabase",
     "RWSQLite",
     "RWFileError",
     "FileSuffixError",
