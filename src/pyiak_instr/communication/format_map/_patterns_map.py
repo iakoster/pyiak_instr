5eb9e0fd5e70a9fbaa8a7cb6354ad2346cda6043
Ivan Kosternoi <iakoster.42@gmail.com>
2023-05-30 15:01:22 +0300

    chore(CI): run nox

--- src/pyiak_instr/communication/format_map/_patterns_map.py
+++ src/pyiak_instr/communication/format_map/_patterns_map.py
@@ -1,3 +1,4 @@
+"""Private module of ``pyiak_instr.communication.format_map``."""
 from pathlib import Path
 from configparser import ConfigParser
 
@@ -8,12 +9,28 @@ from .types import PatternsMapABC
 __all__ = ["PatternsMap"]
 
 
-class PatternsMap(PatternsMapABC):
+class PatternsMap(PatternsMapABC[MessagePattern]):
+    """
+    Class which store message patterns.
+    """
 
     _pattern_type = MessagePattern
 
     @staticmethod
     def _get_pattern_names(path: Path) -> list[str]:
+        """
+        Get pattern names for file in `path` via configparser.
+
+        Parameters
+        ----------
+        path : Path
+            path to file.
+
+        Returns
+        -------
+        list[str]
+            list of pattern names.
+        """
         parser = ConfigParser()
         parser.read(path)
         return parser.sections()
